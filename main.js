476df7c6b8e55a09f3e7170a82ed402a79b953df
Kseniya Biarezina <ksenia-2010@tut.by>
2022-12-02 20:00:46 +0300

    refactor: bind js object and view

--- main.js
+++ main.js
@@ -1,7 +1,80 @@
+let aTasks = [];
+
+const localStorageKey = "taskHist";
+
+const getLS = () => {
+    const tasksHist = localStorage.getItem(localStorageKey);
+    aTasks = JSON.parse(tasksHist || "[]"); 
+
+};
+
+const setLS = () => {
+    localStorage.setItem(localStorageKey, JSON.stringify(aTasks));
+};
+
+window.addEventListener('beforeunload', setLS);
+window.addEventListener('load', getLS);
+
+const render = () => {
+    const list = document.querySelector("#tasks");
+    list.innerHTML = "";
+    aTasks.forEach(element => {
+        addTaskToView(element, list);
+    });
+};
+
+const addTaskToView = (oTask, list) => {
+    const taskEl = document.createElement("div");
+    taskEl.classList.add("task");
+    const taskContentEl = document.createElement("div");
+    taskContentEl.classList.add("content");
+    taskEl.appendChild(taskContentEl);
+    const taskInputEl = document.createElement("input");
+    taskInputEl.classList.add("text");
+    taskInputEl.type = "text";
+    taskInputEl.value = oTask.text;
+    taskInputEl.setAttribute("readonly", "readonly");
+    taskContentEl.appendChild(taskInputEl);
+    const taskActionsEl = document.createElement("div");
+    taskActionsEl.classList.add("actions");
+    const taskEditEl = document.createElement("button");
+    taskEditEl.classList.add("edit");
+    taskEditEl.innerHTML = "Edit";
+    const taskDeleteEl = document.createElement("button");
+    taskDeleteEl.classList.add("delete");
+    taskDeleteEl.innerHTML = "Delete";
+
+    taskActionsEl.appendChild(taskEditEl);
+    taskActionsEl.appendChild(taskDeleteEl);
+    taskEl.appendChild(taskActionsEl);
+    list.appendChild(taskEl);
+
+    taskEditEl.addEventListener('click', () => {
+        if (taskEditEl.innerText.toLowerCase() == "edit") {
+            taskInputEl.removeAttribute("readonly");
+            taskInputEl.focus();
+            taskEditEl.innerText = "Save"
+        } else {
+            taskInputEl.setAttribute("readonly", "readonly");
+            taskEditEl.innerText = "Edit";
+            oTask.text = taskInputEl.value;
+        }
+    })
+
+    taskDeleteEl.addEventListener('click', () => {
+        list.removeChild(taskEl);
+
+        let index = aTasks.indexOf(oTask);
+        aTasks.splice(index, 1);
+    })
+
+}
+
 window.addEventListener('load', () => {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
-    const listEl = document.querySelector("#tasks");
+
+    render();
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -12,46 +85,10 @@ window.addEventListener('load', () => {
             return;
         }
 
-        const taskEl = document.createElement("div");
-        taskEl.classList.add("task");
-        const taskContentEl = document.createElement("div");
-        taskContentEl.classList.add("content");
-        taskEl.appendChild(taskContentEl);
-        const taskInputEl = document.createElement("input");
-        taskInputEl.classList.add("text");
-        taskInputEl.type = "text";
-        taskInputEl.value = task;
-        taskInputEl.setAttribute("readonly", "readonly");
-        taskContentEl.appendChild(taskInputEl);
-        const taskActionsEl = document.createElement("div");
-        taskActionsEl.classList.add("actions");
-        const taskEditEl = document.createElement("button");
-        taskEditEl.classList.add("edit");
-        taskEditEl.innerHTML = "Edit";
-        const taskDeleteEl = document.createElement("button");
-        taskDeleteEl.classList.add("delete");
-        taskDeleteEl.innerHTML = "Delete";
+        aTasks.push({ text: task });
 
-        taskActionsEl.appendChild(taskEditEl);
-        taskActionsEl.appendChild(taskDeleteEl);
-        taskEl.appendChild(taskActionsEl);
-        listEl.appendChild(taskEl);
-
+        render();
         input.value = "";
 
-        taskEditEl.addEventListener('click', () => {
-            if (taskEditEl.innerText.toLowerCase() == "edit") {
-                taskInputEl.removeAttribute("readonly");
-                taskInputEl.focus();
-                taskEditEl.innerText = "Save"
-            } else {
-                taskInputEl.setAttribute("readonly", "readonly");
-                taskEditEl.innerText = "Edit";
-            }
-        })
-
-        taskDeleteEl.addEventListener('click', () => {
-            listEl.removeChild(taskEl);
-        })
     })
 })
